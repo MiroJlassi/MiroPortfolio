a26eba09cf356b4dbe33f0d6c3bfe4a98f5f1413
Marwen <106754847+MiroJlassi@users.noreply.github.com>
2022-06-12 21:05:13 +0100

    0.1.3

--- nthg/index.js
+++
@@ -1,8 +0,0 @@
-function copyToClipboard() {
-  var copyText = document.getElementById("mail").innerHTML;
-  navigator.clipboard.writeText(copyText).then(() => {
-    // Alert the user that the action took place.
-    // Nobody likes hidden stuff being done under the hood!
-    alert("Copied to clipboard");
-  });
-}
